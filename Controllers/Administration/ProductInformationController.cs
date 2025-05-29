3365ab472c501256312a15677dfa6990909010a6
GanjiSanketh <167700147+GanjiSanketh@users.noreply.github.com>
2025-05-29 16:22:11 +0530

    Added code for API's of Product admin screen

--- Controllers/Administration/ProductInformationController.cs
+++ Controllers/Administration/ProductInformationController.cs
@@ -14,19 +14,44 @@ namespace LayerApi.Contrllers.Administration.ProductInformationController
             _productInformationRepository = productInformationRepository;
         }
 
-        [HttpGet("GetAllProductInformations")]
         //[Authorize]
+        [HttpGet("GetAllProductInformations")]
         public async Task<List<ProductInformation>> GetAllProductInformations()
         {
             List<ProductInformation> productInformation = await _productInformationRepository.GetAllProductInformations();
             return productInformation;
         }
-        [HttpGet("GetActiveProductInformations")]
         //[Authorize]
+        [HttpGet]
+        [Route("GetActiveProductInformations")]
         public async Task<List<ProductInformation>> GetActiveProductInformations()
         {
             List<ProductInformation> productInformation = await _productInformationRepository.GetActiveProductInformations();
             return productInformation;
+        }
+        //[Authorize]
+        [HttpGet]
+        [Route("GetProductsById")]
+        public async Task<List<ProductInformation>> GetProductsById(int productId)
+        {
+            List<ProductInformation> productInformation = await _productInformationRepository.GetProductsById(productId);
+            return productInformation;
+        }
+        //[Authorize]
+        [HttpPost]
+        [Route("SaveProductDetails")]
+        public async Task<bool> SaveProductDetails([FromBody] ProductInformation productInformation)
+        {
+            bool isSaved = await _productInformationRepository.SaveProductDetails(productInformation);
+            return isSaved;
+        }
+        //[Authorize]
+        [HttpGet]
+        [Route("DeleteProductInformations")]
+        public async Task<bool> DeleteProductInformations(int productId)
+        {
+            bool isUpdated = await _productInformationRepository.DeleteProductInformations(productId);
+            return isUpdated;
         }
     }
 }
